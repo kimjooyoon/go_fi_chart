cd802f8859c3680ebe8308369e5721f3b9414312
kimjooyoon <asketeddy@gmail.com>
2025-03-07 11:49:40 +0900

    refactor: Asset 도메인의 에러 처리 개선

--- services/asset/internal/domain/errors.go
+++ services/asset/internal/domain/errors.go
@@ -2,14 +2,9 @@ package domain
 
 import (
 	"fmt"
-)
+	"net/http"
 
-// 에러 타입 상수
-const (
-	ErrAssetNotFound      = "ERROR_ASSET_NOT_FOUND"
-	ErrAssetAlreadyExists = "ERROR_ASSET_ALREADY_EXISTS"
-	ErrAssetInvalidData   = "ERROR_ASSET_INVALID_DATA"
-	ErrAssetDeleted       = "ERROR_ASSET_DELETED"
+	commonerrors "github.com/aske/go_fi_chart/internal/common/errors"
 )
 
 // AssetNotFoundError 자산을 찾을 수 없을 때 반환되는 에러입니다.
@@ -22,7 +17,11 @@ func (e AssetNotFoundError) Error() string {
 }
 
 func (e AssetNotFoundError) Code() string {
-	return ErrAssetNotFound
+	return commonerrors.ErrorCodeAssetNotFound
+}
+
+func (e AssetNotFoundError) StatusCode() int {
+	return http.StatusNotFound
 }
 
 // NewAssetNotFoundError 자산을 찾을 수 없는 에러를 생성합니다.
@@ -40,7 +39,11 @@ func (e AssetAlreadyExistsError) Error() string {
 }
 
 func (e AssetAlreadyExistsError) Code() string {
-	return ErrAssetAlreadyExists
+	return commonerrors.ErrorCodeAssetAlreadyExists
+}
+
+func (e AssetAlreadyExistsError) StatusCode() int {
+	return http.StatusConflict
 }
 
 // NewAssetAlreadyExistsError 자산이 이미 존재하는 에러를 생성합니다.
@@ -59,7 +62,11 @@ func (e AssetInvalidDataError) Error() string {
 }
 
 func (e AssetInvalidDataError) Code() string {
-	return ErrAssetInvalidData
+	return commonerrors.ErrorCodeAssetInvalidData
+}
+
+func (e AssetInvalidDataError) StatusCode() int {
+	return http.StatusBadRequest
 }
 
 // NewAssetInvalidDataError 자산 데이터가 유효하지 않은 에러를 생성합니다.
@@ -79,10 +86,41 @@ func (e AssetDeletedError) Error() string {
 
 // Code 에러 코드를 반환합니다.
 func (e AssetDeletedError) Code() string {
-	return ErrAssetDeleted
+	return "ERROR_ASSET_DELETED"
+}
+
+// StatusCode 에러의 HTTP 상태 코드를 반환합니다.
+func (e AssetDeletedError) StatusCode() int {
+	return http.StatusBadRequest
 }
 
 // NewAssetDeletedError는 새로운 AssetDeletedError를 생성합니다.
 func NewAssetDeletedError(assetID string) error {
 	return AssetDeletedError{AssetID: assetID}
+}
+
+// 에러 타입 확인 유틸리티 함수들
+
+// IsAssetNotFound는 주어진 에러가 AssetNotFoundError 타입인지 확인합니다.
+func IsAssetNotFound(err error) bool {
+	var e AssetNotFoundError
+	return commonerrors.As(err, &e)
+}
+
+// IsAssetAlreadyExists는 주어진 에러가 AssetAlreadyExistsError 타입인지 확인합니다.
+func IsAssetAlreadyExists(err error) bool {
+	var e AssetAlreadyExistsError
+	return commonerrors.As(err, &e)
+}
+
+// IsAssetInvalidData는 주어진 에러가 AssetInvalidDataError 타입인지 확인합니다.
+func IsAssetInvalidData(err error) bool {
+	var e AssetInvalidDataError
+	return commonerrors.As(err, &e)
+}
+
+// IsAssetDeleted는 주어진 에러가 AssetDeletedError 타입인지 확인합니다.
+func IsAssetDeleted(err error) bool {
+	var e AssetDeletedError
+	return commonerrors.As(err, &e)
 }
